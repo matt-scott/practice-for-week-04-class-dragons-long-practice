eb426060fea64359c856d10c35c7e95301ad0f8e
Matt Scott <mattscott251@gmail.com>
2023-08-08 15:08:51 -0500

    completed project

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,24 @@
 // Your code here
+class Dragon {
+  constructor (name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return (`${this.name} breathes fire everywhere! BURN!!!!`);
+  }
+
+  static getDragons(...dragons) {
+    let dragonNames = dragons.map((element) => {
+      return element.name;
+    });
+    return dragonNames;
+  }
+}
+
+module.exports = Dragon;
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,21 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class FriendlyDragon extends Dragon {
+    constructor (name, color, lifeGoals, friend) {
+        super (name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach((element) => {
+            console.log(`${this.name} likes to ` + element);
+        });
+    }
+
+    helpsPeople() {
+        return(`${this.name} helps their friend ${this.friend}`);
+    }
+}
+
+module.exports = FriendlyDragon;
